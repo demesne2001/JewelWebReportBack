9c9c5ca56ed136550481cb5ac43909a8b35098b9
patelom0910 <patelom0910@gmail.com>
2024-04-16 11:59:46 +0530

    update

--- DAL/DBConfig.py
+++ DAL/DBConfig.py
@@ -22,7 +22,7 @@ WRconnection = (
 class Connection(Enum):
     LiveConnection=f'DRIVER=ODBC Driver 18 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;Encrypt=no;Connection Timeout=30;'
     # LiveConnection=f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server2};DATABASE={database2};UID={username2};PWD={password2};'
-    Connection=f'DRIVER=SQL Server;SERVER={server2};DATABASE={database2};UID={username2};PWD={password2};'
+    Connection=f'DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username2};PWD={password};'
     
 # WRconnection = (
 #     f'DRIVER=ODBC Driver 18 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;Encrypt=no;Connection Timeout=30;')
