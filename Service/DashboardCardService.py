ef87d09858dea702a4d1088c34a4170b231137b2
patelom0910 <patelom0910@gmail.com>
2024-05-01 18:22:06 +0530

    update

--- Service/DashboardCardService.py
+++ Service/DashboardCardService.py
@@ -74,7 +74,7 @@ def GetCardValue(input:CardandChartInput):
         else:
             param+=f"@Grouping='{input.Grouping}'"
         print('param',param)
-        result.lstResult=DBConfig.CBDExecuteDataReader(param,"Wr_Dashboard_GetCard","GetCardValue")
+        result.lstResult=DBConfig.CDBExecuteDataReader(param,"Wr_Dashboard_GetCard","GetCardValue")
     except  Exception as E:        
         CommanScript.ErrorLog("GetCardValue",DBConfig.spParam(input),"Wr_Dashboard_GetCard",E)
         result.HasError=True
