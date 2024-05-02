80b8a44de8a91f1e10eecdd2f5307a76567f612d
patelomserverpc <patelom0910@gmail.com>
2024-05-02 11:56:28 +0530

    updateeeee

--- DAL/DBConfig.py
+++ DAL/DBConfig.py
@@ -124,7 +124,7 @@ def CDBExecuteDataReader(param,spname,MethodNname):
     if(jwtBearer.CDBConnectionstring ==""):
         connection=pyodbc.connect(Connection.LiveConnection.value)
     else:
-        print('payload',CDBConnectionstring)
+        print('om patel',CDBConnectionstring)
         print(Connection.CDBConnection.value,'cHECK')
         connection=pyodbc.connect(Connection.CDBConnection.value)  
     try:
